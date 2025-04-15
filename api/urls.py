0eb8f4be71a99a4af2f30c5c7393a86fe84291aa
Coker Richard <coker1229@gmail.com>
2025-04-15 16:33:04 +0100

    fix(urls.py):fix endpoint url

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/process/', views.ProcessAPIView.as_view(), name='process'),
-    path('api/status/<str:task_id>/', views.TaskStatusAPIView.as_view(), name='task_status'),
+    path('process', views.ProcessAPIView.as_view(), name='process'),
+    path('status/<str:task_id>', views.TaskStatusAPIView.as_view(), name='task_status'),
 ]
