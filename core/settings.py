add127cba264cdebc906d00eda5bb8c22e7213e0
Coker Richard <coker1229@gmail.com>
2025-04-15 16:21:31 +0100

    chore(settings.py):remove trailing slash

--- core/settings.py
+++ core/settings.py
@@ -26,6 +26,7 @@ DEBUG = True
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -120,6 +121,15 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+APPEND_SLASH = False 
+
+# Celery settings
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 # Security settings for production
 if IS_PRODUCTION:
