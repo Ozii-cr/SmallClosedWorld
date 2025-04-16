1c04f2943c9fa86fe7a228ce4106f964c5c67459
Coker Richard <coker1229@gmail.com>
2025-04-16 15:28:06 +0100

    feat(urls.py):add api documentation endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from . import views
 
 urlpatterns = [
     path('process', views.ProcessAPIView.as_view(), name='process'),
     path('status/<str:task_id>', views.TaskStatusAPIView.as_view(), name='task_status'),
+    
+    #api documentation
+    path('schema', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
