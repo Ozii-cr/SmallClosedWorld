f3a4e83b33a4a3b2ecb73a332852fdce0622d580
Coker Richard <coker1229@gmail.com>
2025-04-18 12:50:20 +0100

    feat(main.tf):create ecr repo

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+module "required_providers" {
+  source               = "/modules/providers"
+  aws_provider_region  = var.aws_region
+  provider_environment = "dev"
+}
+
+################################################################################
+# Ecr repositories
+################################################################################
+module "ecr_repositories" {
+  source = "/modules/ecr-repository"
+
+  repositories = {
+    scw-app = "scw-app"
+  }
+
+  image_tag_mutability = "MUTABLE"
+  scan_on_push         = false
+}
