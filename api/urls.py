c24286fc8b75ba588f3f875dc11694a34f5e2375
Coker Richard <coker1229@gmail.com>
2025-04-18 02:31:38 +0100

    chore(urls):include trailing slash in endpoints

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,12 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 from . import views
 
 urlpatterns = [
-    path('process', views.ProcessAPIView.as_view(), name='process'),
-    path('status/<str:task_id>', views.TaskStatusAPIView.as_view(), name='task_status'),
+    path('process/', views.ProcessAPIView.as_view(), name='process'),
+    path('status/<str:task_id>/', views.TaskStatusAPIView.as_view(), name='task_status'),
     
     
     #api documentation
-    path('schema', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
