667226289c7faf00d60aef1072cf081847e21e48
Coker Richard <coker1229@gmail.com>
2025-04-19 07:55:20 +0100

    feat(varibales.tf):add dev server sensitive variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,16 @@
 variable "aws_region" {
   default = "ap-south-1"
 }
+
+variable "ami_id" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the servers."
+  default     = "ami-0f1dcc636b69a6438"
+
+}
+
+variable "ssh_public_key_dev" {
+  description = "Public SSH key for Scw server"
+  sensitive   = true
+
+}
