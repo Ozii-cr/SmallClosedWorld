fa140badd5f34492c94b9075b76225346219fe0e
Coker Richard <coker1229@gmail.com>
2025-04-19 07:30:29 +0100

    feat(main.tf):create dev vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,26 @@ module "ecr_repositories" {
   image_tag_mutability = "MUTABLE"
   scan_on_push         = false
 }
+
+
+################################################################################
+# Vpc with public subnets
+################################################################################
+module "dev_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.10.0"
+
+  name = "scw-dev-vpc"
+  cidr = "10.1.0.0/16"
+
+  azs = [
+    "${var.aws_region}a",
+    "${var.aws_region}b"
+  ]
+
+  public_subnets = ["10.1.101.0/24", "10.1.102.0/24"]
+
+  enable_nat_gateway = false
+  single_nat_gateway = false
+  enable_vpn_gateway = false
+}
